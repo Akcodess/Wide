38bd1e152c72d6924c4d8c2c4ebcdfd9ae946568
Akhil kumar biswal <akhilkumar785300@gmail.com>
2025-10-23 15:03:27 +0530

    register dto edit

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -2,18 +2,18 @@ import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
 import { Transform, Type } from 'class-transformer';
 
 export class RegisterDto {
-  @IsNotEmpty({ message: 'Tenant code cannot be empty.' })
-  @IsString({ message: 'Tenant code must be a string.' })
+  @IsNotEmpty()
+  @IsString()
   @Transform(({ value }) => value?.trim())
   tenantCode: string;
 
-  @IsNotEmpty({ message: 'Auth code cannot be empty.' })
-  @IsString({ message: 'Auth code must be a string.' })
+  @IsNotEmpty()
+  @IsString()
   @Transform(({ value }) => value?.trim())
   authCode: string;
 
-  @IsNotEmpty({ message: 'Role cannot be empty.' })
-  @IsString({ message: 'Role must be a string.' })
+  @IsNotEmpty()
+  @IsString()
   @Transform(({ value }) => value?.trim())
   role: string;
 }
